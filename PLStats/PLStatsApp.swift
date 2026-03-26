bff5c2a12c6224c0e707556efb4ec7f92f36c95e
hpadi02 <hugop4420@gmail.com>
2026-03-25 23:32:44 -0500

    Initial Commit

---
+++ PLStats/PLStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLStatsApp.swift
+//  PLStats
+//
+//  Created by Hugo Padilla on 3/25/26.
+//
+
+import SwiftUI
+
+@main
+struct PLStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PLStatsTests/PLStatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  PLStatsTests.swift
+//  PLStatsTests
+//
+//  Created by Hugo Padilla on 3/25/26.
+//
+
+import Testing
+@testable import PLStats
+
+struct PLStatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
